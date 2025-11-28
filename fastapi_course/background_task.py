9f61cad3f9bffa841265b68d63d644d6721f4fc1
Erzhan-ish <isenbekoerzan1@gmail.com>
2025-11-29 00:27:46 +0600

    background_tasks | file

---
+++ fastapi_course/background_task.py
@@ -0,0 +1,22 @@
+import asyncio
+import time
+from fastapi import FastAPI, BackgroundTasks
+
+app = FastAPI()
+
+
+def sync_task():
+    time.sleep(3)
+    print("Отправлен email")
+
+
+async def async_task():
+    await asyncio.sleep(3)
+    print("Сделан запрос в сторонний API")
+
+@app.post("/")
+async def some_route(bg_tasks: BackgroundTasks):
+    ...
+    # asyncio.create_task(async_task()) # фоновая задача async
+    bg_tasks.add_task(sync_task) # фоновая задача sync
+    return {"ok": True}
